5f0eafb20f3cf26ff43237ec2284c8e4f3297e5c
AyshanRzayeva <ayshanar@code.edu.az>
2024-03-19 10:34:10 -0700

    Ayshans lab

---
+++ String/new/new.js
@@ -0,0 +1,86 @@
+//let name = 'Ayshan'
+//let surname = 'Rzayeva'
+//let fullname = name + ' ' + surname
+//console.log(fullname);
+
+
+//const user = {
+  //firstName : 'Ayshan',
+  //surname : 'Rzayeva',
+  //age : 24,
+  //uni : 'ADNSU',
+  //address : {
+    //city : 'Baku',
+    //street : 'X.Memmedov',
+    //build : 'Sharur MTK'
+  //}
+//}
+//user.uni = 'ADNSU, AzTU',
+//delete user.uni
+//delete user.salary
+//console.log(user.address.build);
+
+//let info = 'Ayshan Rzayeva Code Academy'
+//let info2 = '2023 - 2024'
+//let result = info.concat(' ',info2)
+//console.log(result);
+
+//let info = 'Ayshan'
+//console.log(info.indexOf('a'));
+//let desc = 'Ayshan Cahad Cahan Geray EliEkber'
+//console.log(desc.includes('Cahad'));
+
+//let desc = 'Ayshan Cahad Cahan Geray EliEkber'
+//console.log(desc.toLocaleLowerCase().includes('GerAY'.toLocaleLowerCase()));
+
+//let info = 'biri var idi biri yox idi'
+//console.log(info.startsWith('b'));
+//console.log(info.endsWith('i'));
+
+
+//let fullName = 'Ayhsan Rzayeva Esed'
+//console.log(fullName.indexOf('R'));
+//console.log(fullName.indexOf('a',10));
+//console.log(fullName.slice(7,14));
+
+
+
+
+
+//let username = 'Ayshan'
+//let surname = 'Rzayeva'
+//console.log(username.index(1,3,5));
+
+
+//let username = 'Ayshan Rzayeva'
+
+
+
+
+
+console.log('Ayshan Rzayeva'.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- String/string.js
+++ String/string.js
@@ -7,6 +7,49 @@
 
 
 
-let username = 'Ayshan'
-let surname = 'Rzayeva'
-console.log
+//let username = 'Ayshan'
+//let surname = 'Rzayeva'
+//console.log
+
+const arr = [4, 8, 9, 1, 4, 9, 10, 11, 33, 21, 11]
+//arr.sort((a,b) => a-b);
+//console.log(arr);
+
+//arr.sort((a,b)=> b-a);
+//console.log(arr);
+
+//arr.reverse();
+//console.log(arr);
+
+//let sum = 0
+//arr.map(item => sum += item);
+//console.log(sum);
+
+//const goster = arr.reduce((acc, item) => acc * item)
+//console.log(goster);
+
+//console.log(arr.find(item => item == 9));
+
+//console.log((arr.filter(item => item === 4)));
+
+//console.log(arr.slice(0,4));
+
+//
+
+console.log(arr.findIndex(item => item ==10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
